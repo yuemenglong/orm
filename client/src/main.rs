4c45744f2207c75ac7c9c752d5c2e637abded057
yuemenglong <yuemenglong@126.com>
2017-02-06 18:29:50 +0800

    add: 分离出curd_test

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,21 +9,28 @@ use entity::*;
 // grant all privileges on *.* to root@'%' identified by 'root';
 // flush privileges;
 
-fn main(){
+fn main() {
     // println!("{:?}", sql::sql_create_table(Person::get_meta()));
-    let mut p = Person::default();
-    p.set_age(100);
-    p.set_name("Tom".to_string());
+
     // p.set_id(10);
     // println!("{:?}", p.get_params());
     let db = ast::open("root", "root", "172.16.16.213", 3306, "test").unwrap();
+    curd_test(&db);
+}
+
+fn curd_test(db: &ast::DB) {
+    let mut p = Person::default();
+    p.set_age(100);
+    p.set_name("Tom".to_string());
     db.drop_table::<Person>().unwrap();
     let ret = db.create_table::<Person>().unwrap();
     let mut p = db.insert(&p).unwrap();
+    println!("{:?}", p);
     let id = p.get_id();
     p.set_name("Dick".to_string());
     let ret = db.update(&p).unwrap();
     let p = db.get::<Person>(p.get_id()).unwrap().unwrap();
+    println!("{:?}", p);
     db.delete(p).unwrap();
     let p = db.get::<Person>(id).unwrap();
     println!("{:?}", p);
