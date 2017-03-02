f30da90a844d78a5a3d833a9018df7fcc2eeb650
yuemenglong <yuemenglong@126.com>
2017-03-02 16:33:41 +0800

    add: 重构部分cascade insert

--- src/db.rs
+++ src/db.rs
@@ -11,6 +11,7 @@ use std::ops::DerefMut;
 // use cond::Cond;
 use entity::Entity;
 use entity::EntityInner;
+use meta::FieldMeta;
 
 pub struct DB {
     pub pool: Pool,
@@ -121,24 +122,15 @@ fn do_insert<C>(inner: &mut EntityInner, conn: &mut C) -> Result<(), Error>
     where C: GenericConnection
 {
     // 遍历pointer
-    for (refer_field, opt) in &inner.pointer_map {
+    for (refer_field, opt) in inner.pointer_map.clone() {
         if opt.is_none() {
-            // lazy load, 在insert的情况下基本不会到这个分支
-            unreachable!();
-        }
-        let refer_inner_rc = opt.as_ref().unwrap().clone();
-        // 拿到该引用对应的meta信息
-        let refer_meta = inner.meta.field_map.get(refer_field).unwrap();
-        // 判断是否需要级联写入
-        if !refer_meta.has_refer_cascade_insert() {
+            // lazy load, 可以直接跳过，说明没有需要操作的
             continue;
         }
-        let refer_id_field = refer_meta.get_pointer_id();
+        let refer_inner_rc = opt.unwrap();
         let mut refer_inner = refer_inner_rc.borrow_mut();
-        try!(do_insert(refer_inner.deref_mut(), conn));
-        // 将refer的id写回原对象对应的refer_id
-        let refer_id = refer_inner.field_map.get("id").unwrap().clone();
-        inner.field_map.insert(refer_id_field, refer_id);
+        let refer_meta = inner.meta.field_map.get(&refer_field).unwrap();
+        try!(do_cascade_insert(inner, refer_inner.deref_mut(), refer_meta, conn));
     }
 
     try!(inner.do_insert(conn));
@@ -165,10 +157,79 @@ fn do_insert<C>(inner: &mut EntityInner, conn: &mut C) -> Result<(), Error>
     Ok(())
 }
 
-fn do_update<C>(inner: &mut EntityInner, conn: &mut C) -> Result<(), Error>
+fn do_cascade_insert<C>(a: &mut EntityInner,
+                        b: &mut EntityInner,
+                        a_b_meta: &FieldMeta,
+                        conn: &mut C)
+                        -> Result<(), Error>
     where C: GenericConnection
 {
-    try!(inner.do_update(conn));
+    if a_b_meta.has_refer_cascade_insert() {
+        try!(do_cascade_insert_pointer(a, b, a_b_meta, conn));
+    }
     Ok(())
 }
 
+fn do_cascade_insert_pointer<C>(a: &mut EntityInner,
+                                b: &mut EntityInner,
+                                a_b_meta: &FieldMeta,
+                                conn: &mut C)
+                                -> Result<(), Error>
+    where C: GenericConnection
+{
+    // insert(b);
+    try!(do_insert(b, conn));
+    // a.b_id = b.id;
+    let b_id_field = a_b_meta.get_pointer_id();
+    let b_id = b.field_map.get("id").unwrap().clone();
+    a.field_map.insert(b_id_field, b_id);
+    Ok(())
+}
+
+fn do_update<C>(inner: &mut EntityInner, conn: &mut C) -> Result<(), Error>
+    where C: GenericConnection
+{
+    // 遍历pointer
+    for (refer_field, opt) in &inner.pointer_map {
+        if opt.is_none() {
+            // lazy load, 可以直接跳过，说明没有需要更新的
+            continue;
+        }
+        let refer_inner_rc = opt.as_ref().unwrap().clone();
+        // 拿到该引用对应的meta信息
+        let refer_meta = inner.meta.field_map.get(refer_field).unwrap();
+        // 判断是否需要级联更新
+        if !refer_meta.has_refer_cascade_update() {
+            continue;
+        }
+        let refer_id_field = refer_meta.get_pointer_id();
+        let mut refer_inner = refer_inner_rc.borrow_mut();
+        try!(do_update(refer_inner.deref_mut(), conn));
+        // 将refer的id写回原对象对应的refer_id, 在更新时似乎可以省略
+        // let refer_id = refer_inner.field_map.get("id").unwrap().clone();
+        // inner.field_map.insert(refer_id_field, refer_id);
+    }
+    // 更新自己
+    try!(inner.do_update(conn));
+    // 需要等a写入后才能写b，因为aid在b上，需要a先有id
+    // update有可能导致insert，所以也一样
+    for (refer_field, opt) in &inner.one_one_map {
+        if opt.is_none() {
+            // 说明没有更新到，基本可以不用管
+            continue;
+        }
+        let refer_inner_rc = opt.as_ref().unwrap().clone();
+        // 拿到该引用对应的meta信息
+        let refer_meta = inner.meta.field_map.get(refer_field).unwrap();
+        // 判断是否需要级联更新
+        if !refer_meta.has_refer_cascade_update() {
+            continue;
+        }
+        let refer_id_field = refer_meta.get_one_one_id();
+        let mut refer_inner = refer_inner_rc.borrow_mut();
+        let self_id = inner.field_map.get("id").unwrap().clone();
+        refer_inner.field_map.insert(refer_id_field.to_string(), self_id);
+        try!(do_insert(refer_inner.deref_mut(), conn));
+    }
+    Ok(())
+}
